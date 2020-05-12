328afdf43503b2fa6da4f34cf40689f93a51e910
iheartkava <65259421+iheartkava@users.noreply.github.com>
2020-05-12 16:46:25 -0700

    Update main.py
    Various custom changes:
     - turn camelCase into proper_case
     - get rid of unneeded functions
     - update various functions to suit my needs

--- main.py
+++ main.py
@@ -1,67 +1,81 @@
 #!/usr/bin/env python3
-import os
-import sys
-import time
-import wikipedia
-
 from lib.constants import BACKOFF, MAX_ATTEMPTS, MAX_STATUS_LEN, TIMEOUT_BACKOFF
-from lib import images
-from lib import mastodon
+from typing import Tuple
 from lib import twitter
+from lib import images
 from lib import words
 
+import wikipedia
+import time
+import sys
+import os
 
 def main():
-    title = searchForTMNT(MAX_ATTEMPTS, BACKOFF)
-    logo = images.getLogo(words.addPadding(title))
-    status_text = "\n".join((title, words.getWikiUrl(title)))
+    title = search_for_stiutk(MAX_ATTEMPTS, BACKOFF)
+    img = images.make_subs(title)
+    status_text = "\n".join((title[0], words.get_wiki_url(title[0])))
 
     if len(status_text) > MAX_STATUS_LEN:
-        status_text = title
+        status_text = title[0]
 
-    _ = twitter.sendTweet(status_text, logo)
-    _ = mastodon.sendToot(status_text, logo)
+    title[0] = status_text
 
+    _ = twitter.send_tweet(title)
 
-def searchForTMNT(attempts=MAX_ATTEMPTS, backoff=BACKOFF):
-    """Loop MAX_ATTEMPT times, searching for a TMNT meter wikipedia title.
+def search_for_stiutk(attempts=MAX_ATTEMPTS, backoff=BACKOFF) -> Tuple[str, int]:
+    """Loop MAX_ATTEMPT times, searching for a STIUTK meter wikipedia title.
 
     Args:
         Integer: attempts, retries remaining.
         Integer: backoff, seconds to wait between each loop.
     Returns:
-        String or False: String of wikipedia title in TMNT meter, or False if
-                         none found.
+        Tuple[str, int] where the first member is the string name
+        of the Wiki article, and the 2nd is which meter it matches;
+        "Somebody That I Used To Know" (1) or
+        "Now You're Just Somebody That I Used To Know" (2)?
     """
     for attempt in range(attempts):
-        print(f"\r{str(attempt * 10)} articles fetched...", end="")
+        print(f"\r{str(attempt * 25)} articles fetched...", end="")
         sys.stdout.flush()
-        title = checkTenPagesForTMNT()
-
-        if type(title) == str and len(title) > 1:
-            print(f"\nMatched: {title}")
-            return title
+        title = crawl_for_stiutk()
 
         time.sleep(backoff)
+
+        if type(title) != None:
+            continue
+
+        try:
+            if type(title[0]) == str and len(title[0]) > 1:
+                print(f"\nMatched: {title[0]}")
+                return title
+        except TypeError:
+            pass
+        except Exception as e:
+            print("Exception: " + str(e))
 
     print(f"\nNo matches found.")
     sys.exit(1)
 
 
-def checkTenPagesForTMNT():
-    """Get 10 random wiki titles, check if any of them isTMNT().
+def crawl_for_stiutk() -> Tuple[str, int]:
+    """Get 10 random wiki titles, check if any of them isSTIUTK().
 
     We grab the max allowed Wikipedia page titles (10) using wikipedia.random().
-    If any title is in TMNT meter, return the title. Otherwise, return False.
+    If any title is in STIUTK meter, return the title and which meter is matched.
+    Otherwise, return False.
 
     Args:
         None
     Returns:
-        String or False: The TMNT compliant title, or False if none found.
+        Tuple[str, int] where the first member is the string name
+        of the Wiki article, and the second is which meter it matches;
+        "Somebody That I Used To Know" (1) or
+        "Now You're Just Somebody That I Used To Know" (2)?
     """
     wikipedia.set_rate_limiting(True)
+
     try:
-        titles = wikipedia.random(10)
+        titles = wikipedia.random(25)
     except wikipedia.exceptions.HTTPTimeoutError as e:
         print(f"Wikipedia timout exception: {e}")
         time.sleep(TIMEOUT_BACKOFF)
@@ -74,10 +88,9 @@ def checkTenPagesForTMNT():
         sys.exit(1)
 
     for title in titles:
-        if words.isTMNT(title):
-            return title
-    return False
-
+        stiutk_type = words.is_stiutk(title)
+        if stiutk_type[0]:
+            return (title, stiutk_type[1])
 
 if __name__ == "__main__":
     main()
