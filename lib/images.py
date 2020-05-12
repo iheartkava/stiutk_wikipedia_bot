f550bc86c0360b31386981be682e369c96e0bba8
iheartkava <65259421+iheartkava@users.noreply.github.com>
2020-05-12 16:50:57 -0700

    Complete rework to locally generate image
    Since we're not doing TMNT and simply adding subtitles to an image, we don't need to use an external side and can do all modification through PIL.

--- lib/images.py
+++ lib/images.py
@@ -1,93 +1,81 @@
-import subprocess
+from lib.constants import (
+    IMG_PATH,
+    SRC_IMG,
+    DEST_IMG
+)
+
+from PIL import Image, ImageDraw, ImageFont
+from typing import Tuple
+from math import floor
 import sys
 
-from lib.constants import LOGO_PATH, CHROME_PATH, SCREENSHOT_PATH
-from PIL import Image, ImageChops
+def make_subs(img: Tuple[str, int]) -> bool:
+    """Get 10 random wiki titles, check if any of them isSTIUTK().
 
+    We grab the max allowed Wikipedia page titles (10) using wikipedia.random().
+    If any title is in STIUTK meter, return the title and which meter is matched.
+    Otherwise, return False.
 
-def getLogo(title: str, chrome=CHROME_PATH):
-    title = title.replace(" ", "_")
+    Args:
+        Tuple[str, int] where the first member is the string name
+        of the Wiki article, and the second is which meter it matches;
+        "Somebody That I Used To Know" (1) or
+        "Now You're Just Somebody That I Used To Know" (2)?
+    Returns:
+        Bool on success or failure.
+    """
+    if (img[1] == 2):
+        full = True
+    else:
+        full = False
 
-    # TODO: Generate logo locally, stop hitting glench.com (sorry glench)
-    chrome_cmd = (
-        f"{chrome} "
-        "--headless "
-        "--disable-gpu "
-        f"--screenshot={SCREENSHOT_PATH} "
-        "--window-size=1280,600 "
-        f'"http://glench.com/tmnt/#{title}"'
-    )
+    image = Image.open(SRC_IMG)
+    font_size = 47
+    msg = img[0]
+    draw = ImageDraw.Draw(image)
 
-    retcode = subprocess.run(chrome_cmd, shell=True).returncode
-    if retcode != 0:
-        sys.stderr.write(f"Chrome subprocess exited with code {retcode}")
-        sys.exit(1)
+    # This is basically the whole reason we carried that Touple around
+    # everywhere... With this, we can match the shorter version but correct
+    # it so it still says "Now You're Just [wiki article finish the rest]"
+    # This might be awful, so I might get rid of it. We'll see.
+    if full:
+        msg = "Now You're Just " + msg
 
-    logo_path = _cropLogo(SCREENSHOT_PATH)
-    _ = _compressPng(logo_path)
-    return logo_path
+    # We want the text to be as big as possible without spilling over
+    # at all, so this loop constantly checks if it's within appropriate
+    # bounds and adjusts the font size accordingly if it's too big or small.
+    des_x = 0
+    while des_x < 10 or des_x > 150:
+        font = ImageFont.truetype(font='/Windows/Fonts/trebucit.ttf', size=font_size)
+        text_w, text_h = draw.textsize(msg, font)
+        img_w, img_h = image.size
 
+        des_x = (img_w - text_w) // 2
+        des_y = img_h - text_h - 50
+        sys.stdout.write("des_x: " + str(des_x) + "\n")
 
-def _compressPng(path):
-    cmd = f"/usr/bin/zopflipng -m -y --lossy_8bit --lossy_transparent {path} {path}"
-    retcode = subprocess.run(cmd, shell=True).returncode
-    if retcode != 0:
-        sys.stderr.write(f"zopfli subprocess exited with code {retcode}")
+        #breakpoint()
+        if des_x < 5:
+            font_size -= 3
+        elif des_x > 50:
+            font_size += 3
+        else:
+            break
+
+    # Some of the ugliest code I think I've ever written :) but it
+    # still works. Most of these lines create a black outline.
+    draw.text((des_x, des_y + 4), msg, font = font, fill = (0,0,0))
+    draw.text((des_x, des_y - 4), msg, font = font, fill = (0,0,0))    
+    draw.text((des_x - 4, des_y), msg, font = font, fill = (0,0,0))
+    draw.text((des_x + 4, des_y), msg, font = font, fill = (0,0,0))
+    draw.text((des_x - 3, des_y - 3), msg, font = font, fill = (0,0,0))
+    draw.text((des_x - 3, des_y + 3), msg, font = font, fill = (0,0,0))
+    draw.text((des_x + 3, des_y - 3), msg, font = font, fill = (0,0,0))
+    draw.text((des_x + 3, des_y + 3), msg, font = font, fill = (0,0,0))
+
+    # Then this line adds the "foreground" white text.
+    draw.text((des_x, des_y), msg, font = font, fill = (255,255,255))
+    image.save(DEST_IMG)
+    image.close()
+
     return True
-
-def _convertToWebp(path):
-    cwebp_cmd = f"/usr/bin/cwebp -q 60 -mt -m 6 -af {path} -o {path}.webp"
-    retcode = subprocess.run(cwebp_cmd, shell=True).returncode
-    if retcode != 0:
-        sys.stderr.write(f"cwebp subprocess exited with code {retcode}")
-        # Not a critical error, just return the png
-        return path
-
-    logo_path = f"{path}.webp"
-    sys.stderr.write(f"path is {logo_path}")
-    return logo_path
-
-
-def _trimWhitespace(im):
-    # calculate bbox of image area
-    bg = Image.new(im.mode, im.size, im.getpixel((0, 0)))
-    diff = ImageChops.difference(im, bg)
-    diff = ImageChops.add(diff, diff, 2.0, -100)
-    bbox = diff.getbbox()
-
-    if bbox:
-        # crop the image and store sizes as variables
-        croppedImage = im.crop(bbox)
-        croppedImageWidth = croppedImage.size[0]
-        croppedImageHeight = croppedImage.size[1]
-
-        # calculate size of output image based on width of cropped image,
-        # 1:1.9 aspect ratio, and side margin pixels
-        SIDE_MARGIN = 30
-        outputImageWidth = croppedImageWidth + (SIDE_MARGIN * 2)
-        outputImageHeight = int(outputImageWidth * 0.52632)
-        outputImageSize = tuple([outputImageWidth, outputImageHeight])
-
-        # create empty image
-        outputImage = Image.new(im.mode, outputImageSize, im.getpixel((0, 0)))
-
-        # calculate positioning of cropped image on empty background, paste
-        x = SIDE_MARGIN
-        y = int((outputImageHeight - croppedImageHeight) / 2)
-        outputImage.paste(croppedImage, (x, y))
-
-        return outputImage
-
-
-def _cropOffTopAndBottom(image_path: str):
-    im = Image.open(image_path)
-    w, h = im.size
-    return im.crop((0, 175, w, h - 100))
-
-
-def _cropLogo(im):
-    logo_path = LOGO_PATH
-    im = _cropOffTopAndBottom(im)
-    im = _trimWhitespace(im)
-    im.save(logo_path)
-    return logo_path
